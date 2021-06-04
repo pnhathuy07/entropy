fb5955c4bb41af280640b5ce6f9d6f484f8d61a2
Phan Nhat Huy <p.nhathuy07@gmail.com>
2021-06-04 11:12:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from functions import inp
 
-from encryption import encode
-from encryption import decode
+from encryption import encode, decode
 from destroy import destroy
 
 selection = inp('Do you want to encode/decode the file or destroy the file completely?', '', '', 'Encode', 'Decode', 'Destroy').upper()
